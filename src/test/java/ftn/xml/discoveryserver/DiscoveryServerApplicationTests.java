de877e8c8561900f72e33dbb7de4a41f2d84b531
mandy94 <majasavic199@gmail.com>
2020-06-29 18:52:43 +0200

    first commit

---
+++ src/test/java/ftn/xml/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.xml.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
